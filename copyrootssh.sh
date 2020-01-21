2cc76503a891bcdc192fac982477569056bbe763
Geordie-R <40744776+Geordie-R@users.noreply.github.com>
2020-01-21 00:18:33 +0000

    Create copyrootssh.sh

---
+++ copyrootssh.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+#**********************************************************#
+#                  copyrootssh.sh                          #
+#**********************************************************#
+read -p "What is your non-root username called?: "username
+mkdir -p /home/$username/.ssh
+cp -rf /root/.ssh/* /home/$username/.ssh/
+chown -R $username:$username /home/$username/.ssh
