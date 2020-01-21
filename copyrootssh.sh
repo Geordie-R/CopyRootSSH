479ad096390f5813b587c59a0597fbc7fd16ac4c
Geordie-R <40744776+Geordie-R@users.noreply.github.com>
2020-01-21 00:29:29 +0000

    Update copyrootssh.sh

--- copyrootssh.sh
+++ copyrootssh.sh
@@ -4,7 +4,7 @@
 #**********************************************************#
 #                  copyrootssh.sh                          #
 #**********************************************************#
-read -p "What is your non-root username called?: "username
+read -p "What is your non-root username called?: " -e username
 mkdir -p /home/$username/.ssh
 cp -rf /root/.ssh/* /home/$username/.ssh/
 chown -R $username:$username /home/$username/.ssh
